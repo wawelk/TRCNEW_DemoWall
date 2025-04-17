97832e160072ecd2bbed4d049b0ce4a6f5212905
wawelk <wawelk@reynco.com>
2025-04-17 10:41:04 -0500

    Updated Network diagram

---
+++ ProjectFiles/NetSolution/raSDK1_NL_AccordionEnableTriggerDelay.cs
@@ -0,0 +1,43 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.RAEtherNetIP;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.CoreBase;
+using FTOptix.OPCUAServer;
+using FTOptix.Retentivity;
+using FTOptix.CommunicationDriver;
+using FTOptix.NetLogic;
+using FTOptix.AuditSigning;
+using FTOptix.EventLogger;
+using FTOptix.Store;
+using FTOptix.Core;
+using FTOptix.Alarm;
+using FTOptix.UI;
+#endregion
+
+public class raSDK1_NL_AccordionEnableTriggerDelay : BaseNetLogic
+{
+    private DelayedTask myDelayedTask;
+
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        myDelayedTask = new DelayedTask(SetDelayOn, 500, LogicObject);
+        myDelayedTask.Start();
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+
+    public void SetDelayOn()
+    {
+        Owner.Owner.GetVariable("_EnableAutoExpand").Value = true;
+    }
+
+}
